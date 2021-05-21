cb62e2853a4250ae317b5080067a6020fbf35fd9
djutras <djutras@gmail.com>
2021-05-20 23:42:37 -0400

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,23 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">1.0.0.5 Next.jssssss 5/20/2021!</a>
+          Welcome to <a href="https://nextjs.org">1.0.0.6 - 5/20/2021!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
+               <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
 
         <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
